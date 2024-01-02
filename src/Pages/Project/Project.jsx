9c1c3ba74de570f7482dfd99e79b7ee67a7e1d79
hossain <guljer.net@gmail.com>
2024-01-02 22:07:05 +0600

    project add

--- src/Pages/Project/Project.jsx
+++ src/Pages/Project/Project.jsx
@@ -1,49 +1,59 @@
 import React, { useState } from "react";
 import Container from "../../Components/Container";
 import BannerHead from "../../Components/BannerHead";
-import ImageProject from '../../assets/1.png';
+import Content from '../../../public/all_product';
 
 function Project() {
-  const [project, setProject] = useState(0);
+  const [project, setProject] = useState(Content);
   const toggler = (id) =>{
-    setProject(id);
+    const items = Content.filter(item=> item.category === id);
+    setProject(items);
   }
+
   return (
     <div>
       <BannerHead title={"My Project"} subTitle={"Project"} />
       <Container>
         <div className="py-10">
-          <div className="border shadow-lg rounded-full lg:w-[50%] w-full mx-auto">
+          <div className="border shadow-lg rounded-full lg:w-[50%] sm:w-[70%] w-full mx-auto">
             <div className="flex items-center justify-between">
-              <div onClick={()=> toggler(0)}>
+              <div onClick={()=> setProject(Content)}>
                 <button
                   className={`lg:px-7 px-3 rounded-full ${project === 0 ? "bg-primary text-white":""} inline-block text-center lg:py-3 py-1 text-[14px]`}
                 >
                   All
                 </button>
               </div>
-              <div onClick={()=> toggler(1)}>
+              <div onClick={()=> toggler('React Js')}>
                 <button
-                  className={`lg:px-7 px-3 rounded-full ${project === 1 ? "bg-primary text-white":""} inline-block text-center lg:py-3 py-1 text-[14px]`}
+                  className={`lg:px-7 px-3 rounded-full ${project === "React Js" ? "bg-primary text-white":""} inline-block text-center lg:py-3 py-1 text-[14px]`}
                 >
                   React Js
                 </button>
               </div>
-              <div onClick={()=> toggler(2)}>
+              <div onClick={()=> toggler('Next.Js')}>
                 <button
-                  className={`lg:px-7 px-3 rounded-full ${project === 2 ? "bg-primary text-white":""} inline-block text-center lg:py-3 py-1 text-[14px]`}
+                  className={`lg:px-7 px-3 rounded-full ${project === "Next.Js" ? "bg-primary text-white":""} inline-block text-center lg:py-3 py-1 text-[14px]`}
                 >
                   Next.Js
                 </button>
               </div>
-              <div onClick={()=> toggler(3)}>
+              <div onClick={()=> toggler('Full Stack')}>
                 <button
-                  className={`lg:px-7 px-3 rounded-full ${project === 3 ? "bg-primary text-white":""} inline-block text-center lg:py-3 py-1 text-[14px]`}
+                  className={`lg:px-7 px-3 rounded-full ${project === "Full Stack" ? "bg-primary text-white":""} inline-block text-center lg:py-3 py-1 text-[14px]`}
                 >
                   Full Stack
                 </button>
               </div>
             </div>
+          </div>
+          <div className="grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-3 gap-5 pt-10">
+            {
+              project.map((items, i) => <div key={i} className="p-5 rounded-md bg-gray-100 shadow-lg">
+                <img src={items?.image} alt="" className="w-full h-auto" />
+                <h4 className="my-3 text-[18px] font-semibold">{items?.name}</h4>
+              </div>)
+            }
           </div>
         </div>
       </Container>
