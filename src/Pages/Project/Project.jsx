f338dc5e328b030f0aac059e5e070fac756e1df5
hossain <guljer.net@gmail.com>
2024-01-02 21:21:38 +0600

    add css class

--- src/Pages/Project/Project.jsx
+++ src/Pages/Project/Project.jsx
@@ -1,40 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../../Components/Container";
 import BannerHead from "../../Components/BannerHead";
 import ImageProject from '../../assets/1.png';
 
 function Project() {
+  const [project, setProject] = useState(0);
+  const toggler = (id) =>{
+    setProject(id);
+  }
   return (
     <div>
       <BannerHead title={"My Project"} subTitle={"Project"} />
       <Container>
         <div className="py-10">
-          <div className="border shadow-lg rounded-full lg:w-[50%] w-full mx-auto w-full">
+          <div className="border shadow-lg rounded-full lg:w-[50%] w-full mx-auto">
             <div className="flex items-center justify-between">
-              <div>
+              <div onClick={()=> toggler(0)}>
                 <button
-                  className={`lg:px-7 px-3 rounded-full inline-block text-center lg:py-3 py-1 text-[14px]`}
+                  className={`lg:px-7 px-3 rounded-full ${project === 0 ? "bg-primary text-white":""} inline-block text-center lg:py-3 py-1 text-[14px]`}
                 >
                   All
                 </button>
               </div>
-              <div>
+              <div onClick={()=> toggler(1)}>
                 <button
-                  className={`lg:px-7 px-3 rounded-full inline-block text-center lg:py-3 py-1 text-[14px]`}
+                  className={`lg:px-7 px-3 rounded-full ${project === 1 ? "bg-primary text-white":""} inline-block text-center lg:py-3 py-1 text-[14px]`}
                 >
                   React Js
                 </button>
               </div>
-              <div>
+              <div onClick={()=> toggler(2)}>
                 <button
-                  className={`lg:px-7 px-3 rounded-full inline-block text-center lg:py-3 py-1 text-[14px]`}
+                  className={`lg:px-7 px-3 rounded-full ${project === 2 ? "bg-primary text-white":""} inline-block text-center lg:py-3 py-1 text-[14px]`}
                 >
                   Next.Js
                 </button>
               </div>
-              <div>
+              <div onClick={()=> toggler(3)}>
                 <button
-                  className={`lg:px-7 px-3 rounded-full inline-block text-center lg:py-3 py-1 text-[14px]`}
+                  className={`lg:px-7 px-3 rounded-full ${project === 3 ? "bg-primary text-white":""} inline-block text-center lg:py-3 py-1 text-[14px]`}
                 >
                   Full Stack
                 </button>
